8501089532e8d4a9b9bf826598eeba7e068230e5
edandresvan <ederandresan@gmail.com>
2023-02-26 11:32:45 -0500

    Finished section 5.2

--- ch05/questionnaire-web/src/types/pagination.rs
+++ ch05/questionnaire-web/src/types/pagination.rs
@@ -19,6 +19,17 @@ pub struct Pagination {
 ///
 /// * `params`: Parameters to limit the set of results to retrieve.
 ///
+/// # Example Usage
+///
+/// ```rust  
+/// let mut query = HashMap::new();
+/// query.insert("start".to_string(), "1".to_string());
+/// query.insert("end").to_string(), "20".to_string());
+///
+/// let pagination = types::pagination::extract_pagination(query).unwrap;
+/// assert_eq!(pagination.start, 1);
+/// assert_eq!(pagination.end, 20);
+/// ```
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, QError> {
   if params.contains_key("start") && params.contains_key("end") {
     let start_index: usize = params
